85ac0da03255b1887915211971547fb4419ccfae
stepansubbotin <stepan.v.subbotin@gmail.com>
2015-07-02 17:04:36 -0500

    Added comments to Piston class

--- src/org/usfirst/frc/team4536/robot/subsystems/Piston.java
+++ src/org/usfirst/frc/team4536/robot/subsystems/Piston.java
@@ -4,29 +4,47 @@ import edu.wpi.first.wpilibj.Solenoid;
 
 public class Piston {
 	
+	
 	Solenoid leftSolenoid;
 	Solenoid rightSolenoid;
 	
+	/**
+	 * @author Stepan
+	 * @param leftSolenoidChannel channel that the left solenoid is plugged into on the roboRIO
+	 * @param rightSolenoidChannel channel that the right solenoid is plugged into on the roboRIO
+	 */
 	public Piston(int leftSolenoidChannel, int rightSolenoidChannel) {
 		leftSolenoid = new Solenoid(leftSolenoidChannel);
 		rightSolenoid = new Solenoid(rightSolenoidChannel);
 	}
 	
+	/**
+	 * @author Stepan
+	 */
 	public void retract() {
 		leftSolenoid.set(true);
 		rightSolenoid.set(false);
 	}
 	
+	/**
+	 * @author Stepan
+	 */
 	public void extend() {
 		leftSolenoid.set(false);
 		rightSolenoid.set(true);
 	}
 	
+	/**
+	 * @author Stepan
+	 */
 	public void flip() {
 		leftSolenoid.set(!leftSolenoid.get());
 		rightSolenoid.set(!rightSolenoid.get());
 	}
 	
+	/**
+	 * @author Stepan
+	 */
 	public boolean isExtended() {
 		return rightSolenoid.get();
 	}
