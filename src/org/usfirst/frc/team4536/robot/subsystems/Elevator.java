a7003460384cb1f08eb4f934306b3572468773a3
AnonymousAnion <liam.tomsonmoylan@gmail.com>
2015-10-03 14:02:48 -0500

    Cleaned up formatting in elevator subsystem and added getCurrent method

--- src/org/usfirst/frc/team4536/robot/subsystems/Elevator.java
+++ src/org/usfirst/frc/team4536/robot/subsystems/Elevator.java
@@ -12,123 +12,138 @@ import edu.wpi.first.wpilibj.DigitalInput;
 
 
 public class Elevator extends Subsystem {
-    
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+	
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
  
- Jaguar elevatorJaguar;
- Encoder elevatorEncoder;
- DigitalInput topLimitSwitch;
- DigitalInput middleLimitSwitch;
- DigitalInput bottomLimitSwitch;
+	Jaguar elevatorJaguar;
+	Encoder elevatorEncoder;
+	DigitalInput topLimitSwitch;
+	DigitalInput middleLimitSwitch;
+	DigitalInput bottomLimitSwitch;
  
- double actualHeight;
- double setHeight;
- double currentHeight;
- double correction;
+	double actualHeight;
+	double setHeight;
+	double currentHeight;
+	double correction;
+	
+	
+	public Elevator (int jaguarChannel, 
+					int encoderAChannel,
+					int encoderBChannel,
+					int topLimitSwitchChannel, 
+					int middleLimitSwitchChannel,
+					int bottomLimitSwitchChannel)
+	{
+		
+		elevatorJaguar = new Jaguar(jaguarChannel);
+		elevatorEncoder = new Encoder(encoderAChannel, encoderBChannel);
+		//For our particular Robot "Zenith" in the 2014-2015 competition The A Channel has only the signal wire
+		topLimitSwitch = new DigitalInput(topLimitSwitchChannel);
+		middleLimitSwitch = new DigitalInput(middleLimitSwitchChannel);
+		bottomLimitSwitch = new DigitalInput(bottomLimitSwitchChannel); 
+	}
  
+	/**
+	 * @ author: Mairead
+	 * @ return: True if switch is pressed
+	 */
+	public boolean topLimitSwitchValue() {
+	 
+		return !topLimitSwitch.get();
+	}
  
- public Elevator (int jaguarChannel, 
-   int encoderAChannel,
-   int encoderBChannel,
-   int topLimitSwitchChannel, 
-   int middleLimitSwitchChannel,
-   int bottomLimitSwitchChannel)
- {
-  elevatorJaguar = new Jaguar(jaguarChannel);
-     elevatorEncoder = new Encoder(encoderAChannel, encoderBChannel);
-     //For our particular Robot "Zenith" in the 2014-2015 competition The A Channel has only the signal wire
-     topLimitSwitch = new DigitalInput(topLimitSwitchChannel);
-     middleLimitSwitch = new DigitalInput(middleLimitSwitchChannel);
-     bottomLimitSwitch = new DigitalInput(bottomLimitSwitchChannel); 
- }
- 
-    /**
-     * @ author: Mairead
-     * @ return: True if switch is pressed
-     */
- public boolean topLimitSwitchValue() {
-  
-  return !topLimitSwitch.get();
- }
- 
- /**
-     * @ author: Mairead
-     * @ return: True if switch is pressed
-     */
- public boolean middleLimitSwitchValue(){
-  return !middleLimitSwitch.get();
- }
- 
- /**
-     * @ author: Mairead
-     * @ return: True if switch is pressed
-     */
- public boolean bottomLimitSwitchValue(){
-  return !bottomLimitSwitch.get();
- }
-   
- /**
-     * @ author: Mairead
-     * @ param verticalThrottle - The throttle input into the elevator motor.
-     *          Values go from -1 to 1
-     *          Positive values make the elevator go up, negative makes the elevator go down 
-     */
- public void drive(double verticalThrottle){
-  
-  //Makes sure that the elevator can only drive when not hitting a limit switch
-  
-     if ((this.topLimitSwitchValue() == true) && (verticalThrottle > 0)){
-      //If the elevator hits the top limit switch it can go down but not up
-      
-      this.drive(0);
-     }
-     else if (((this.bottomLimitSwitchValue() == true) 
-       || (this.middleLimitSwitchValue() == true)) 
-       && (verticalThrottle < 0)){
-      //If the elevator hits the middle or bottom limit switch it can go up but not down
-      
-      this.drive(0);
-     }
-     
-     else
-      elevatorJaguar.set(-verticalThrottle);
- }
- 
- 
- public void setActualHeight(double actualHeight){
-  correction = actualHeight - elevatorEncoder.get()/Constants.TICKS_PER_INCHES;
- }
- 
- public void update(){
-  currentHeight = correction + elevatorEncoder.get()/Constants.TICKS_PER_INCHES;
-  
-  if (this.topLimitSwitchValue()) {
-   setActualHeight(Constants.TOP_LIMIT_SWITCH_HEIGHT);
-  }
-  
-  if (this.middleLimitSwitchValue()) {
-   setActualHeight(Constants.MIDDLE_LIMIT_SWITCH_HEIGHT);
-  }
-  
-  if (this.bottomLimitSwitchValue() == true) {
-   setActualHeight(Constants.BOTTOM_LIMIT_SWITCH_HEIGHT);
-  }
-   
+	/**
+	 * @ author: Mairead
+	 * @ return: True if switch is pressed
+	 */
+	public boolean middleLimitSwitchValue(){
 
- }
+		return !middleLimitSwitch.get();
+	}
  
- public double getEncoderHeight(){
-  return elevatorEncoder.getDistance();
- }
- 
- public double getCurrentHeight(){
-  return currentHeight;
- }
+	/**
+	 * @ author: Mairead
+	 * @ return: True if switch is pressed
+	 */
+	public boolean bottomLimitSwitchValue(){
+		
+		return !bottomLimitSwitch.get();
+	}
+   
+	/**
+	 * @ author: Mairead
+	 * @ param verticalThrottle - The throttle input into the elevator motor.
+	 *          Values go from -1 to 1
+	 *          Positive values make the elevator go up, negative makes the elevator go down 
+	 */
+	public void drive(double verticalThrottle){
+		
+		//Makes sure that the elevator can only drive when not hitting a limit switch
+		
+		if ((this.topLimitSwitchValue() == true) && (verticalThrottle > 0)){
+			//If the elevator hits the top limit switch it can go down but not up
+			
+			this.drive(0);
+		}
+		else if (((this.bottomLimitSwitchValue() == true) 
+				|| (this.middleLimitSwitchValue() == true)) 
+				&& (verticalThrottle < 0)){
+			//If the elevator hits the middle or bottom limit switch it can go up but not down
+			
+			this.drive(0);
+		}
+		
+		else {
+			
+			elevatorJaguar.set(-verticalThrottle);
+		}
+	}
+	
+	
+	public void setActualHeight(double actualHeight){
+		
+		correction = actualHeight - elevatorEncoder.get()/Constants.TICKS_PER_INCHES;
+	}
+	
+	public void update(){
+	 
+		currentHeight = correction + elevatorEncoder.get()/Constants.TICKS_PER_INCHES;
+		
+		if (this.topLimitSwitchValue()) {
+			
+			setActualHeight(Constants.TOP_LIMIT_SWITCH_HEIGHT);
+		}
+		
+		if (this.middleLimitSwitchValue()) {
+			
+			setActualHeight(Constants.MIDDLE_LIMIT_SWITCH_HEIGHT);
+		}
+		
+		if (this.bottomLimitSwitchValue() == true) {
+			
+			setActualHeight(Constants.BOTTOM_LIMIT_SWITCH_HEIGHT);
+		}
+	}
+	
+	public double getEncoderHeight(){
+		
+		return elevatorEncoder.getDistance();
+	}
+	
+	public double getCurrentHeight(){
+		
+		return currentHeight;
+	}
+	
+	public double getCurrent() {
+		
+		return Utilities.getCurrent(RobotMap.ELEVATOR_PDP);
+	}
 
-    public void initDefaultCommand() {
-        // Set the default command for a subsystem here
-        //setDefaultCommand(new MySpecialCommand()); 
-    }
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here
+		//setDefaultCommand(new MySpecialCommand()); 
+	}
 }
 
